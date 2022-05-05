b8654652b54c3c79b53957bb8edf5fc79bd91a8b
Alok Kushwaha <95715756+thefriendlyprogrammer@users.noreply.github.com>
2022-05-05 17:48:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overrinduing
+{
+
+    class Test
+    {
+        public  void test()
+        {
+            Console.WriteLine("this is test method Prt");
+        }
+    }
+    class Test1 : Test
+    {
+        public override void test()
+        {
+            Console.WriteLine("this is test method child");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1 obj = new Test1();
+            obj.test();
+            Console.ReadLine();
+
+
+        }
+    }
+}
